d1400230ccd7e34a69de09b70f79d7b0a4078efa
juan2089 <129207110+juan2089@users.noreply.github.com>
2025-08-07 16:32:40 +0100

    Add files via upload

--- ui.R
+++ ui.R
@@ -134,7 +134,7 @@ ui <- dashboardPage(
                        checkboxGroupInput("datacheck",label=tags$span("Check an option if you are NOT using HDExaminer output file",bsButton("testingbutton",label="",style="info",icon=icon("info"),size="extra-small")),
                                           c("WATERS/Dynamix output file", "Custom .csv output file"))),
               bsPopover(id = "testingbutton",title = HTML("<b> Important </b>"),
-                        content = paste0("If you are not using an HDExaminer output file, please select the option that fits your data. Check WATERS/Dynamix if you are using a Dinamix output file or check Custom csv for any other type of file. Note: for custom .csv, you will need to manually format your data as in the csv file available for download in the bottom right link."),
+                        content = paste0("If you are not using an HDExaminer output file, please select the option that fits your data. Check WATERS/Dynamix if you are using a Dynamix output file or check Custom csv for any other type of file. Note: for custom .csv, you will need to manually format your data as in the csv file available for download in the bottom right link."),
                         placement = "right",
                         trigger = "hover",
                         options = list(container = "body")
